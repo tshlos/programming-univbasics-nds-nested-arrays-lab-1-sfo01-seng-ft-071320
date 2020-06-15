de0376fc3302399573d588605f5ac7c402c4151c
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-15 01:17:26 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -39,9 +39,8 @@ def sorted_matrix
       [ "Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant" ],
       [ "Strawberries", "Potatoes", "Grapes", "Avocadoes","Asparagus" ]
   ]
-  produce_storage_room[0].sort
-  produce_storage_room[1].sort
-  
+  sorted = produce_storage_room.sort
+
 end
 
 def matrix_lookup(matrix, row, column)
